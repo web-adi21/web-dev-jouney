ca0e62830dd9d63ea9351c3a3b65884a2cef3833
Aditya Ojha <adityaojha2043@gmail.com>
2026-02-08 12:44:14 +0530

    Doing the activity of JS 10

--- JS DOM/app.js
+++ JS DOM/app.js
@@ -2,3 +2,7 @@ let button = document.createElement('button');
 button.innerText = "Click Me";
 let body = document.querySelector('body');
 body.append(button);
+button.id = 'btn';
+let btn = document.querySelector('#btn');
+btn.style.backgroundColor = 'blue';
+btn.style.color = 'white';
