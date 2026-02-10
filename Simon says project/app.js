fe679d19b5e195f737afc537c54eb18c12e10604
Aditya Ojha <adityaojha2043@gmail.com>
2026-02-10 19:29:03 +0530

    made it to chose a random color and move it accordingily

--- Simon says project/app.js
+++ Simon says project/app.js
@@ -1,10 +1,35 @@
+let num;
+let color;
+let red = document.querySelector(".red-btn");
+let blue = document.querySelector(".blue-btn");
+let green = document.querySelector(".green-btn");
+let orange = document.querySelector(".orange-btn");
+function flash(a) {
+a.style.height = "0px";
+setTimeout(() => {a.style.height = "130px"} , 500);
+};
 function startgame() {
-  let color = Math.floor(Math.random()*10 + 1);
-  if (color > 4) {
-    while(color > 4) {
-      color = color - 4;
+  let num = Math.floor(Math.random()*10 + 1);
+  if (num > 4) {
+    while(num > 4) {
+      num = num - 4;
     };
   };
+  console.log(num);
+  if (num == 1) {
+    color = red;
+    flash(color);
+  } else if (num == 2) {
+    color = blue;
+    flash(color);
+  } else if (num == 3) {
+    color = green;
+    flash(color);
+  } else if (num == 4) {
+    color = orange;
+    flash(color);
+  }
+
   console.log(color);
 }
 let body = document.querySelector("body");
