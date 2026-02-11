973cc20bc229e0103d1886aee1e7aacf5632405a
Aditya Ojha <adityaojha2043@gmail.com>
2026-02-11 14:04:50 +0530

    couldnt complete but did all i could for the simon says game

--- Simon says project/app.js
+++ Simon says project/app.js
@@ -1,36 +1,37 @@
-let num;
-let color;
-let red = document.querySelector(".red-btn");
-let blue = document.querySelector(".blue-btn");
-let green = document.querySelector(".green-btn");
-let orange = document.querySelector(".orange-btn");
-function flash(a) {
-a.style.height = "0px";
-setTimeout(() => {a.style.height = "130px"} , 500);
-};
-function startgame() {
-  let num = Math.floor(Math.random()*10 + 1);
-  if (num > 4) {
-    while(num > 4) {
-      num = num - 4;
-    };
-  };
-  console.log(num);
-  if (num == 1) {
-    color = red;
-    flash(color);
-  } else if (num == 2) {
-    color = blue;
-    flash(color);
-  } else if (num == 3) {
-    color = green;
-    flash(color);
-  } else if (num == 4) {
-    color = orange;
-    flash(color);
-  }
+let gameSeq = [];
+let userSeq = [];
 
-  console.log(color);
+let btns = ["orange" , "red" , "blue" , "green"]
+
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector(".h2");
+
+document.addEventListener("keypress" , function() {
+  if (started == false) {
+    console.log("game has started");
+    started = true;
+    levelup();
+  }
+});
+
+function btnFlash(btn) {
+  btn.classList.add("flash");
+  setTimeout(function () {
+    btn.classList.remove("flash");
+  },300);
 }
-let body = document.querySelector("body");
-body.addEventListener("keypress", startgame);
+function levelup() {
+level++;
+h2.innerText = `Level ${level}`;
+
+let randIdx = Math.floor(Math.random() * 3);
+let randcolor = btns[randIdx];
+let randBtn = document.querySelector(`.${randcolor}`);
+console.log(randIdx);
+console.log(randcolor);
+console.log(randBtn);
+btnFlash(randBtn);
+}
+
