ae5654370474552f453ca5d172688899aaf0f7ae
Aditya Ojha <adityaojha2043@gmail.com>
2026-03-25 11:47:01 +0530

    USING EXPRESSJS

--- backend/expressjs/index.js
+++ backend/expressjs/index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 
 const app = express();
+let port = 8080;
 
-console.dir(app);
+app.listen(port, () => {
+  console.log(`app is listening on port ${port}`);
+})
+
+app.use((req,res) => {
+  console.log("req was recieved");
+});
