7219bc34e82afa6d885a77d413c4ccc841a614dc
Aditya Ojha <adityaojha2043@gmail.com>
2026-03-31 21:24:37 +0530

    setting up home/ejs

--- backend/EJSdir/index.js
+++ backend/EJSdir/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
-
+const path = require("path");
 const port = 8080;
 
 app.set("view engine", "ejs");
 
+app.set("views", path.join(__dirname, "/views"));
+
+
 app.get("/", (req , res) => {
-  res.send("this is home");
-} );
+  res.render("home.ejs")
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${8080}`);
 });
+
+app.get("/ankush",(req , res) => {
+  res.send("yOU CAME TO DIFF DIRECTORY")
+})
