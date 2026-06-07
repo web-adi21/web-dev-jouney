2bcc6476f4bbfb0e60d99cb7588375c23fb01eb4
Aditya Ojha <adityaojha2043@gmail.com>
2026-06-07 23:18:49 +0530

    making first schema and model

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -9,4 +9,12 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/test');
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-};
+}; 
+
+const userSchema = new mongoose.Schema({
+  name : String,
+  email : String,
+  age : Number,
+});
+
+const User = mongoose.model('User', userSchema);
