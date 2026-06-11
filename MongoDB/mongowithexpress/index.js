b21c12e21b0b87f75ab3334807bf6fb2ac913ed9
Aditya Ojha <adityaojha2043@gmail.com>
2026-06-11 14:18:01 +0530

    Setting up folder for normal use of express with mongo db

---
+++ MongoDB/mongowithexpress/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("express");
+const app = express();
+const path = require('path');
+
+main()
+  .then(() => {
+    console.log("connection succesfully established");
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}; 
+
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/",(req , res) => {
+  res.send("Working Root");
+});
+
+app.listen(8080, () => {
+  console.log("app is listening");
+});
