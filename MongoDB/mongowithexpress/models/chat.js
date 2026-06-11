d184f14b401a8b75ffce503f9957b0fb977ce7ac
Aditya Ojha <adityaojha2043@gmail.com>
2026-06-11 23:06:05 +0530

    building the chat schema

--- MongoDB/mongowithexpress/models/chat.js
+++ MongoDB/mongowithexpress/models/chat.js
@@ -1,1 +1,21 @@
 const mongoose = require('mongoose');
+
+const chatSchema = new mongoose.Schema({
+  fromUser : {
+    type: String,
+    required:true
+  },
+   toUser : {
+    type: String,
+    required:true
+  },
+   content : {
+    type: String
+  },
+   created_at : {
+    type: Date,
+    required:true
+  }
+});
+
+const Chat = mongoose.model("Chat", chatSchema)
