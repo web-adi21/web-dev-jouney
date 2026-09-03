77aef0dfe62c6a4285c1447c47631232bbb48341
Aditya Ojha <adityaojha2043@gmail.com>
2026-09-03 16:49:36 +0530

    doing setup for activity

--- REACT/learning-react-app/src/App.jsx
+++ REACT/learning-react-app/src/App.jsx
@@ -1,19 +1,14 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+
 import './App.css'
 import "./HelloMessage"
-import HelloMessage from './HelloMessage'
+
 
 function App() {
 
 return(
-      <>
-      <HelloMessage userName="Adi" textColor="blue" />
-      <HelloMessage userName="Alice" textColor="green" />
-      <HelloMessage userName="Bob" textColor="red" />
-      </>
+    <>
+      
+    </>
 )
 }
 
--- REACT/learning-react-app/src/HelloMessage.jsx
+++
@@ -1,9 +0,0 @@
-function HelloMessage({userName,textColor}) {
-  return(
-    <h1 style={{color: textColor}}>
-      Hello {userName}!
-    </h1>
-  )
-}
-
-export default HelloMessage;
