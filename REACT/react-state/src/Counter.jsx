921e52cfe6cca066489f87b0e3b49cf02590a2f9
Aditya Ojha <adityaojha2043@gmail.com>
2026-09-10 10:25:53 +0530

    creating counter using states

--- REACT/react-state/src/Counter.jsx
+++ REACT/react-state/src/Counter.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 
 export default function Counter() {
-  let [stateVariable,setStateVariable] = useState(10);                                 
+  
+  let [count , setCount] = useState(0);
+
+  let incCount = () => {
+    setCount(count+1);
+    console.log(count);
+  }
+  
   return (
     <div>
-      <h3>Count = </h3>
-      <button>Increase Count</button>
+      <h3>Count = {count}</h3>
+      <button onClick={incCount}>Increase Count</button>
     </div>
   )
 }
